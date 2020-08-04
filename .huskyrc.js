96ec0f0726c38e7c13259a7ba1aa84ab7e798abe
Marcos Natan <natan@deltahub.com.br>
2020-08-04 00:23:59 -0300

    build(commitzen): run test on commit

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,7 +2,7 @@ const tasks = arr => arr.join(' && ');
 
 module.exports = {
   hooks: {
-    'pre-commit': tasks(['yarn test:unit']),
+    'pre-commit': tasks(['yarn test']),
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
   },
 };
